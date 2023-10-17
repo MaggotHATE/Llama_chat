4680e8891c335bd3f9eee0330fac651b3d96a984
MaggotHATE <clay1326@gmail.com>
2023-10-17 23:25:16 +0500

    Latest commits, special tokens should be fixed (clblast - not so much)

--- base/common.h
+++ base/common.h
@@ -6,8 +6,6 @@
 
 #include "sampling.h"
 
-#define LOG_NO_FILE_LINE_FUNCTION
-
 #include <string>
 #include <vector>
 #include <random>
@@ -136,12 +134,14 @@ struct llama_context_params llama_context_params_from_gpt_params(const gpt_param
 std::vector<llama_token> llama_tokenize(
   const struct llama_context * ctx,
            const std::string & text,
-                        bool   add_bos);
+                        bool   add_bos,
+                        bool   special = false);
 
 std::vector<llama_token> llama_tokenize(
     const struct llama_model * model,
            const std::string & text,
-                        bool   add_bos);
+                        bool   add_bos,
+                        bool   special = false);
 
 // tokenizes a token into a piece
 // should work similar to Python's `tokenizer.id_to_piece`
